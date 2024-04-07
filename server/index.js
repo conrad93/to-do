22b496af56cc566b464da2e2e4bf58d3bf320498
Conrad Nunes <nunes.conrad93@gmail.com>
2024-04-07 19:16:10 +0530

    fin

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: false}));
-app.use(express.static(path.resolve(__dirname, "../client/dist")));
+app.use(express.static(path.resolve(__dirname, "../client/dist/to-do")));
 
 app.get("/get-data", async (req, res) => {
     try {
