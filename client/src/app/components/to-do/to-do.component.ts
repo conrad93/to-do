73a133504438c0c3a9b20bd6107064eafffbf789
Conrad Nunes <nunes.conrad93@gmail.com>
2024-04-07 18:33:38 +0530

    fin

--- client/src/app/components/to-do/to-do.component.ts
+++ client/src/app/components/to-do/to-do.component.ts
@@ -16,11 +16,41 @@ export class ToDoComponent implements OnInit, OnDestroy {
   week: Week[] = [];
   currentDate: string = '';
   weeklyItems: ToDo[] = [];
+  weekData: any = {};
+  total = 0;
   
   constructor(private todoService: ToDoService, private modalService: NgbModal){}
 
   ngOnInit(): void {
     this.getToday();
+  }
+
+  processItems(){
+    this.total = 0;
+    this.weekData = {};
+    this.week.forEach(e => {
+      this.weekData[e.fullDate] = {
+        count: 0,
+        day: e.day,
+        items: []
+      };
+    });
+    this.weeklyItems.forEach(e => {
+      if(this.weekData[e.date]){
+        this.weekData[e.date]["count"] += 1;
+        this.weekData[e.date]["items"].push(e);
+      }
+    });
+
+    for(let data in this.weekData){
+      this.weekData[data]["items"].push({addItem: true});
+      this.weekData[data]["count"] += 1;
+      this.total = Math.max(this.total, this.weekData[data]["count"]);
+    }
+  }
+
+  getCounterArray(){
+    return new Array(this.total);
   }
   
   getWeek(){
@@ -28,6 +58,7 @@ export class ToDoComponent implements OnInit, OnDestroy {
     this.isLoading = true;
     this.week = this.todoService.getWeek(this.currentDate);
     this.weeklyItems = this.todoService.getWeeklyItems(this.currentDate);
+    this.processItems();
     this.isLoading = false;
   }
   
@@ -58,6 +89,7 @@ export class ToDoComponent implements OnInit, OnDestroy {
           this.weeklyItems.push(item);
         }
         this.handleSave();
+        this.processItems();
       }
     });
   }
@@ -75,18 +107,23 @@ export class ToDoComponent implements OnInit, OnDestroy {
           this.weeklyItems.push(item);
         }
         this.handleSave();
+        this.processItems();
       }
     });
   }
 
-  deleteItem(i: number){
-    this.weeklyItems.splice(i, 1);
+  deleteItem(id: any){
+    let index = this.weeklyItems.findIndex(mainItem => +mainItem.id === +id);
+    this.weeklyItems.splice(index, 1);
     this.handleSave();
+    this.processItems();
   }
 
-  changeStatus(i: number){
-    this.weeklyItems[i].status = !this.weeklyItems[i].status;
+  changeStatus(id: any){
+    let index = this.weeklyItems.findIndex(mainItem => +mainItem.id === +id);
+    this.weeklyItems[index].status = !this.weeklyItems[index].status;
     this.handleSave();
+    this.processItems();
   }
 
   handleSave(){
